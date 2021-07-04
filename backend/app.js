bf6923544fe62d3ed153cdb99553f5bf212d6e59
Timur Marokko <proofblame@gmail.com>
2021-07-04 18:05:25 +0300

    localhost

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
-    res.send(200);
+    res.send(200)
   }
   next();
 });
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,52 +1,52 @@
 class Auth {
-    constructor(baseURL) {
-        this.baseURL = baseURL;
-    }
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
 
-    _getResponseData(res) {
-        if (res.ok) return res.json();
-        return res
-            .json()
-            .then((res) => Promise.reject(new Error(res.message || res.error)));
-    }
+  _getResponseData(res) {
+    if (res.ok) return res.json();
+    return res
+      .json()
+      .then((res) => Promise.reject(new Error(res.message || res.error)));
+  }
 
-    register(email, password) {
-        return fetch(`${this.baseURL}/signup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                password,
-                email
-            })
-        }).then(this._getResponseData);
-    }
+  register(email, password) {
+    return fetch(`${this.baseURL}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    }).then(this._getResponseData);
+  }
 
-    login(email, password) {
-        return fetch(`${this.baseURL}/signin`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                password,
-                email
-            })
-        }).then(this._getResponseData);
-    }
+  login(email, password) {
+    return fetch(`${this.baseURL}/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    }).then(this._getResponseData);
+  }
 
-    getUserData(jwt) {
-        return fetch(`${this.baseURL}/users/me`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `${jwt}`
-            }
-        }).then(this._getResponseData);
-    }
+  getUserData(jwt) {
+    return fetch(`${this.baseURL}/users/me`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${jwt}`,
+      },
+    }).then(this._getResponseData);
+  }
 }
 
-const auth = new Auth("https://api.proofblame.nomoredomains.icu");
-// const auth = new Auth("http://localhost:3000");
+// const auth = new Auth("https://api.proofblame.nomoredomains.icu");
+const auth = new Auth('http://localhost:3000');
 
 export default auth;
